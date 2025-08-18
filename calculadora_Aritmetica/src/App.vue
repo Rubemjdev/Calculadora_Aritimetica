a9bce83774d83c237b6f7bf7a7f21345f27b884c
Rubem Barbosa <Rubemjdev@gmail.com>
2025-08-18 09:51:22 -0300

    Adicionado o tipo da operação

--- calculadora_Aritmetica/src/App.vue
+++ calculadora_Aritmetica/src/App.vue
@@ -1,23 +1,37 @@
 <script setup>
-  import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 </script>
 
 <template>
   <div class="container">
-    <header class="text-center mt-3 mb-3 bg-light p-3 rounded-5">
+    <header class="text-center mt-3 mb-3 bg-light p-3 rounded-5 shadow-sm">
       <h1>Olá</h1>
       <p>
         Selecione o tipo da operação e digite os números a serem calculados
       </p>
     </header>
-    <div class="card p-4 shadow-sm">
+    <div class="card p-4 shadow-lg">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-4 mb-4">
+            <label class="form-label" for="operacao">
+              Selecione a Operação:
+            </label>
+            <select id="operacao" class="form-select">
+              <option value="soma">Soma</option>
+              <option value="sub">Subtração</option>
+              <option value="multi">Multiplicação</option>
+              <option value="divi">Divisão</option>
+            </select>
+          </div>
+        </div>
+      </div>
       <div class="row">
         <div class="col-md-4 mb-3">
           <label for="numero1" class="form-label">Número 1</label>
           <input type="number" id="numero1" class="form-control" placeholder="Digite o primeiro número">
         </div>
-
         <div class="col-md-4 mb-3">
           <label for="numero2" class="form-label">Número 2</label>
           <input type="number" id="numero2" class="form-control" placeholder="Digite o segundo número">
@@ -32,6 +46,4 @@
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
